663f3abc80a1988591c0e4e9b2fc3b0c6ffc5ca7
alecscscscs <alexarghius2005@gmail.com>
2025-12-24 13:33:16 +0200

    Refactor service worker event listeners for clarity

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
 const CACHE = "pixel-fishing-v1";
-const ASSETS = ["./","./index.html","./manifest.json","./sw.js","./icon-192.png","./icon-512.png"];
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
